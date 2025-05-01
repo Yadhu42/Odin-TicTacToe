dd1d1d21b95146cf07b29ce9d70a1a2014dd230d
yadhuks42 <yadhuks42@gmail.com>
2025-05-01 17:26:41 +0000

    Css styling

--- script.js
+++ script.js
@@ -162,6 +162,22 @@ const displayControl= ( () => {
 
             if(typeof(state)===`string`){
                 if(state===`Tie`){
+                    let square = document.querySelectorAll(`.gridSquare`);
+                    square.forEach((sq) =>{
+                        if(sq.innerHTML.includes(`tics`)){
+                            sq.innerHTML=`<svg class="tics" id="${event.target.id}" style="filter: drop-shadow(3px 5px 2px rgb(0,0,0,0.4));" width="150px" height="150px" viewBox="0 -0.5 21 21" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="#000000">
+                                            <g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><defs> </defs> <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"> <g id="Dribbble-Light-Preview" transform="translate(-419.000000, -240.000000)" class="ticMarkL"> <g id="icons" transform="translate(56.000000, 160.000000)"> 
+                                            <polygon  id="${event.target.id}" points="375.0183 90 384 98.554 382.48065 100 373.5 91.446 364.5183 100 363 98.554 371.98065 90 363 81.446 364.5183 80 373.5 88.554 382.48065 80 384 81.446"> 
+                                            </polygon> </g> </g> </g> </g>
+                                        </svg>` 
+                        }
+                        else if(sq.innerHTML.includes(`toes`)){
+                            sq.innerHTML=`<svg class="toes"  id="${event.target.id}" style="filter: drop-shadow(3px 5px 2px rgb(0,0,0,0.4));" width="150px" height="150px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier">
+                                                <path id="${event.target.id}" d="M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" class="tacMarkL" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g>
+                                            </svg>`
+                        }
+                    })
                     container.removeEventListener(`click`,winState,{
                         capture:true
                     });
@@ -175,7 +191,7 @@ const displayControl= ( () => {
                 if(winner.playerMark===`X`){
                     state.forEach((id) =>{
                     let square = document.querySelector(`.sq${id}`)
-                    square.innerHTML = `<svg class="tics" id="${event.target.id}" width="150px" height="150px" viewBox="0 -0.5 21 21" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="#000000">
+                    square.innerHTML = `<svg style="filter: drop-shadow(3px 5px 2px rgb(0,0,0,0.4));" class="tics" id="${event.target.id}" width="150px" height="150px" viewBox="0 -0.5 21 21" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="#000000">
                                             <g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><defs> </defs> <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"> <g id="Dribbble-Light-Preview" transform="translate(-419.000000, -240.000000)" class="ticMarkW"> <g id="icons" transform="translate(56.000000, 160.000000)"> 
                                             <polygon  id="${event.target.id}" points="375.0183 90 384 98.554 382.48065 100 373.5 91.446 364.5183 100 363 98.554 371.98065 90 363 81.446 364.5183 80 373.5 88.554 382.48065 80 384 81.446"> 
                                             </polygon> </g> </g> </g> </g>
@@ -185,7 +201,7 @@ const displayControl= ( () => {
                 else if(winner.playerMark===`O`){
                     state.forEach((id) =>{
                         let square = document.querySelector(`.sq${id}`)
-                        square.innerHTML = `<svg class="toes" id="${event.target.id}" width="150px" height="150px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        square.innerHTML = `<svg style="filter: drop-shadow(3px 5px 2px rgb(0,0,0,0.4));" class="toes" id="${event.target.id}" width="150px" height="150px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                 <g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier">
                                                 <path id="${event.target.id}" d="M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" class="tacMarkW" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g>
                                             </svg>`
